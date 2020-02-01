0dba69b13aab86c7c62f2add1eb679e93e628daf
Dzenan Jupic <info@dzenanjupic.de>
2020-02-02 00:17:56 +0100

    smal terminal output update

--- main.py
+++ main.py
@@ -5,28 +5,44 @@ from num2words import num2words
 from email_list import *
 from data import *
 
+MAX_ERR = 10
 
-def get_int(min_value=0, max_value=1, input_msg=None):
+
+def get_int(min_value=0, max_value=1, input_msg=None, default=None):
     while True:
         try:
-            ans = int(input(input_msg))
-            if ans < min_value or ans > max_value:
+            ans = input(input_msg)
+            if ans == "":
+                return default
+            else:
+                ans = int(ans)
+
+            if min_value < ans < max_value:
+                return ans
+            else:
                 print(f"input needs to be between {min_value} and {max_value}")
-                continue
-            break
         except KeyboardInterrupt:
             exit()
         except:
             print("please input a number")
 
-    return ans
 
-
-addresses = get_int(0, len(to), "How many addresses do you want to send mails to?:\n")
+addresses = get_int(
+    0,
+    len(to),
+    f"How many addresses do you want to send mails to?:\n[min: 0, max: {len(to)}, default: {len(to)}]\n",
+    len(to)
+)
 if addresses == 0:
     exit()
 to = to[:addresses]
-rounds = get_int(0, 1000, "How many times do you want to send the mail?:\n")
+
+rounds = get_int(
+    0,
+    1000,
+    "How many times do you want to send the mail?:\n[min: 0, max: 1000, default: 1]\n",
+    1
+)
 if rounds == 0:
     exit()
 
@@ -51,27 +67,43 @@ message = msg.as_string()
 print(
     "\n\nSummary:"
     f"\n\tsubject: {subject}"
-    f"\n\tbody: {body}"
+    "\n\tbody: " + body.strip('\n').strip('\t') +
     f"\n\tfile: {files} x {round(file_size/1024/1024)}MB ({file_name})"
 )
 
 # send message
 print("\n\n\n\nstart sending\n\n")
-with SMTP("smtp.gmail.com", 587) as server:
-    server.starttls()
-    server.login(me, password)
+err_count = 0
+try:
+    with SMTP("smtp.gmail.com", 587) as server:
+        server.starttls()
+        server.login(me, password)
 
-    for i in range(1, rounds+1):
-        print(f"start round {i}/{rounds}")
+        while err_count < MAX_ERR:
+            try:
+                for i in range(1, rounds+1):
+                    print(f"start round {i}/{rounds}")
 
-        for receiver in to:
-            print(f"\tsend email to: {receiver}")
-            server.sendmail(me, receiver, message)
+                    for receiver in to:
+                        print(f"\tsend email to: {receiver}")
+                        server.sendmail(me, receiver, message)
+                break
+            except KeyboardInterrupt:
+                rounds = i
+                break
+            except:
+                err_count += 1
 
-print(
-    "\n\nfinished sending mails "
-    f"{num2words(rounds)} {'times' if rounds>1 else 'time'} "
-    f"to {num2words(len(to))} {'receivers' if len(to)>1 else 'receiver'} "
-    "successfully!"
-    f"\ntotal send data: {round((file_size*rounds*len(to))/1024/1024, 2)}MB"
-)
+except KeyboardInterrupt:
+    pass
+except:
+    print("something went wrong!")
+    exit()
+finally:
+    print(
+        "\n\nfinished sending mails "
+        f"{num2words(rounds)} {'times' if rounds>1 else 'time'} "
+        f"to {num2words(len(to))} {'receivers' if len(to)>1 else 'receiver'} "
+        "successfully!"
+        f"\ntotal send data: {round((file_size*rounds*len(to))/1024/1024, 2)}MB"
+    )
